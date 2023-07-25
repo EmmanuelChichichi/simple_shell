24efbe06ed199306ca9815e2cfca0048079b9237
Ndivhu45 <ndivhu2456@gmail.com>
2023-07-25 07:16:51 -0700

    better

---
+++ shell_18.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * my_hand_envs - Prints the env variables.
+ * Return: empty function
+ */
+
+void my_hand_envs(void)
+{
+	char **nv = environ;
+
+	while (*nv)
+	{
+		printf("%s\n", *nv);
+		nv++;
+	}
+}
